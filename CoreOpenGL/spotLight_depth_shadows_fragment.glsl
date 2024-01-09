8ff53f677a644ca348ed6bce97ddef5179e13340
INDRA\pdortegon <pdortegon@eservicios.indra.es>
2024-01-09 07:33:44 +0100

    Antes de refactorizacion

--- CoreOpenGL/spotLight_depth_shadows_fragment.glsl
+++
@@ -1,8 +0,0 @@
-#version 330 core 
-
-layout(location = 0) out vec3 fragmentdepth;
-
-void main(){ 
-    fragmentdepth = vec3(gl_FragCoord.z); 
-}
-
--- CoreOpenGL/spotLight_depth_shadows_vertex.glsl
+++
@@ -1,8 +0,0 @@
-#version 330 core
-
-layout(location = 0) in vec3 in_Position;
-uniform mat4 shadowMVP;
-
-void main() {
-    gl_Position = shadowMVP * vec4(in_Position, 1);
-}
